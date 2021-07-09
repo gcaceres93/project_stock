9b3aa9a9478fe851408dd03e177e4e711bc66a8d
junior <junior08gonzalez>
2021-07-09 18:24:42 -0300

    Actualizacion del calculo de la diferencia moneda

--- rate_currency_bi/models/account_invoice.py
+++ rate_currency_bi/models/account_invoice.py
@@ -8,7 +8,7 @@ class account_invoice_bi(models.Model):
 
     diferencia_moneda = fields.Monetary(store=True, readonly=True, compute='_compute_diferecia_moneda')
 
-    @api.depends('currency_id','price_subtotal')
+    @api.depends('price_subtotal')
     def _compute_diferecia_moneda(self):
         for rec in self:
             #campo type o inv
@@ -17,12 +17,22 @@ class account_invoice_bi(models.Model):
             # in_refund = Nota de Credito emitida por Proveedor
             # out_refund = Nota de Credito enviada al cliente
             # if rec.invoice_id.type == 'out_invoice':
-            if rec.currency_id.id == 3:
-                rec.diferencia_moneda = rec.price_subtotal
+            # moneda = rec.env['res.currency'].search([('id','=',3)])
+            factura = self.env['account_invoice'].search([('id','=',rec.invoice_id)])
+            if factura.date_invoice:
+                moneda = self.env['res.currency.rate'].search([('currency_id', '=', 3),
+                                                                 ('name', '=',factura.date_invoice )])
+                # if rec.currency_id.id == 3:
+                #     rec.diferencia_moneda = rec.price_subtotal
+                # else:
+                #     if rec.price_subtotal > 0 and rec.currency_id.rate_bi > 0:
+                # rec.diferencia_moneda = rec.price_subtotal / rec.currency_id.rate_bi
+                if moneda and moneda.inverse_rate:
+                    rec.diferencia_moneda = rec.price_subtotal / moneda.inverse_rate
+                else:
+                    rec.diferencia_moneda = rec.price_subtotal
             else:
-                if rec.price_subtotal > 0 and rec.currency_id.rate_bi > 0:
-                    rec.diferencia_moneda = rec.price_subtotal / rec.currency_id.rate_bi
-
+                rec.diferencia_moneda = rec.price_subtotal
 
 
 
