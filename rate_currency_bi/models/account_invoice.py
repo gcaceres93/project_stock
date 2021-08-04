b981bb3b689e2588ab1216af1d0b7321230c517c
junior <junior08gonzalez>
2021-08-04 15:58:02 -0300

    Actualizacion Modulo rate_currency_bi by Arturo

--- rate_currency_bi/models/account_invoice.py
+++ rate_currency_bi/models/account_invoice.py
@@ -10,7 +10,7 @@ class account_invoice_bi(models.Model):
 
     @api.depends('price_subtotal')
     def _compute_diferecia_moneda(self):
-        for rec in self:
+
             #campo type o inv
             # in_invoice = Factura de Proveedor
             # out_invoice = Factura de Cliente
@@ -18,25 +18,27 @@ class account_invoice_bi(models.Model):
             # out_refund = Nota de Credito enviada al cliente
             # if rec.invoice_id.type == 'out_invoice':
             # moneda = rec.env['res.currency'].search([('id','=',3)])
-            if rec.invoice_id.currency_id.id == 3:
-                rec.diferencia_moneda = rec.price_subtotal
-            else:
-                factura = self.env['account.invoice'].search([('id','=',rec.invoice_id.id)])
+        if self.invoice_id.currency_id:
+            if self.invoice_id.currency_id.id == 3:
+                self.diferencia_moneda = self.price_subtotal
+            elif self.invoice_id.currency_id.id != 3:
+                factura = self.env['account.invoice'].search([('id','=',self.invoice_id.id)])
                 if factura.date_invoice:
                     moneda = self.env['res.currency.rate'].search([('currency_id', '=', 3),
-                                                                     ('name', '=',factura.date_invoice )])
-                    # if rec.currency_id.id == 3:
-                    #     rec.diferencia_moneda = rec.price_subtotal
-                    # else:
-                    #     if rec.price_subtotal > 0 and rec.currency_id.rate_bi > 0:
-                    # rec.diferencia_moneda = rec.price_subtotal / rec.currency_id.rate_bi
+                                                                         ('name', '=',factura.date_invoice)])
+                        # if rec.currency_id.id == 3:
+                        #     rec.diferencia_moneda = rec.price_subtotal
+                        # else:
+                        #     if rec.price_subtotal > 0 and rec.currency_id.rate_bi > 0:
+                        # rec.diferencia_moneda = rec.price_subtotal / rec.currency_id.rate_bi
                     if moneda and moneda.inverse_rate:
-                        rec.diferencia_moneda = rec.price_subtotal / moneda.inverse_rate
+                        self.diferencia_moneda = self.price_subtotal / moneda.inverse_rate
                     else:
-                        rec.diferencia_moneda = rec.price_subtotal
+                        self.diferencia_moneda = self.price_subtotal
                 else:
-                    rec.diferencia_moneda = rec.price_subtotal
-
+                    self.diferencia_moneda = self.price_subtotal
+        else:
+            self.diferencia_moneda = 0
 
 
 
