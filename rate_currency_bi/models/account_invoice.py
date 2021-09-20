e40f7a1a38f8560cecd27a84fcfb84c05a4035ce
junior08gonzalez <52926548+junior08gonzalez@users.noreply.github.com>
2021-09-20 15:26:05 -0300

    Update account_invoice.py

--- rate_currency_bi/models/account_invoice.py
+++ rate_currency_bi/models/account_invoice.py
@@ -1,30 +1,40 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class account_invoice_bi(models.Model):
     _inherit = 'account.invoice.line'
 
-    diferencia_moneda = fields.Monetary(store=True, readonly=True, compute='_compute_diferecia_moneda')
+    diferencia_moneda = fields.Monetary(store=True, compute='_compute_diferecia_moneda')
     
     @api.one
-    @api.depends('currency_id','price_subtotal')
+    @api.depends('price_subtotal','quantity','invoice_line_tax_ids')
     def _compute_diferecia_moneda(self):
+        _logger.info("#######_compute_diferecia_moneda######")
+        #campo type o inv
+        # in_invoice = Factura de Proveedor
+        # out_invoice = Factura de Cliente
+        # in_refund = Nota de Credito emitida por Proveedor
+        # out_refund = Nota de Credito enviada al cliente
+        # if rec.invoice_id.type == 'out_invoice':
+        # moneda = rec.env['res.currency'].search([('id','=',3)])
         for rec in self:
-            #campo type o inv
-            # in_invoice = Factura de Proveedor
-            # out_invoice = Factura de Cliente
-            # in_refund = Nota de Credito emitida por Proveedor
-            # out_refund = Nota de Credito enviada al cliente
-            if rec.invoice_id.type == 'out_invoice':
-                if rec.currency_id.id == 3:
-                    rec.diferencia_moneda = rec.price_subtotal
-               # else:
-                #    if rec.price_subtotal != 0:
-                 #       rec.diferencia_moneda = rec.price_subtotal / rec.currency_id.rate_bi
+            if rec.currency_id:
+                _logger.info(rec.currency_id.id)
+                _logger.info("#####if rec.currency_id:#####")
+                _logger.info(rec.invoice_id.id)
+                if rec.invoice_id.date_invoice:
+                    if rec.currency_id.id == 2:
+                        rec.diferencia_moneda = rec.price_subtotal
+                            
+                    else:
+                         tasa_moneda = rec.env['res.currency.rate'].search([('currency_id', '=', 2),
+                                                                              ('name', '=',rec.invoice_id.date_invoice)])
 
-
-
-
-
+                         if tasa_moneda and tasa_moneda.set_venta:
+                              _logger.info("#######tasa_moneda and tasa_moneda.set_venta######")
+                              rec.diferencia_moneda = rec.price_subtotal / tasa_moneda.set_venta
